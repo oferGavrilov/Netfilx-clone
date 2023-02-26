f07bb05c7cd151306565a0d64bae7c2999c8150e
ofer <ofergavri@gmail.com>
2023-02-26 23:39:04 +0200

    added styles for global css

--- cmps/Header.tsx
+++ cmps/Header.tsx
@@ -1,17 +1,17 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+// import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 function Header() {
     return (
-        <header>
+        <header >
             <div className='flex items-center space-x-2 md:space-x-10'>
                 <img
-                    src="https://rb.gy/ulxxee"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png'
                     alt="logo"
                     width={100}
                     height={100}
                     className="cursor-pointer object-contain"
                 />
 
-                <ul className='flex '>
+                <ul className='flex'>
                     <li className='headerLink'>Home</li>
                     <li className='headerLink'>TV Shows</li>
                     <li className='headerLink'>Movies</li>
@@ -22,7 +22,7 @@ function Header() {
             </div>
 
             <div>
-                <MagnifyingGlassIcon className="hidden sm:inline" />
+                {/* <MagnifyingGlassIcon className="hidden sm:inline" /> */}
             </div>
         </header>
     )
