e1ea23989e1971f4e0d85ffe3e26b51f408835ee
Idan David <idand2000@gmail.com>
2023-03-26 17:35:32 +0300

    fix bug

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,46 +1,64 @@
 import Image from 'next/image'
 import React, { useEffect, useState, useRef } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modal'
 
 import { Movie } from '../models/main.model'
-import { movieService } from '../services/move.service'
-import HoverImgModal from './HoverImgModal'
 
 interface Props {
       movie: Movie
+      debounce: Function
+      setPos(pos: Object): void
 }
 
-function Thumbnail({ movie }: Props) {
+function Thumbnail({ movie, debounce, setPos }: Props) {
       const [movieDetails, setMovieDetails] = useState<Movie | null>(null)
-      const [isHover, setIsHover] = useState(false)
-      const [pos, setPos] = useState({x: 0, y: 0})
+      const [showModal, setShowModal] = useRecoilState(modalState)
+      const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
       const elArticle = useRef<HTMLDivElement>(null)
-      useEffect(() => {
-            loadMovieDetails()
-            onSetPos()
-      },[])
+      const timeout: any = useRef()
 
-      async function loadMovieDetails() {
-            const movieDetails = await movieService.getMovieById(movie.id) as Movie
-            setMovieDetails(movieDetails)
-      }
+      useEffect(() => {
+            // loadMovieDetails()
+            // onSetPos()
+      }, [])
+
+      // async function loadMovieDetails() {
+      //       const movieDetails = await movieService.getMovieById(movie.id) as Movie
+      //       setMovieDetails(movieDetails)
+      // }
 
       function onSetPos() {
-            if(elArticle.current) {
-                  const x = elArticle.current.getBoundingClientRect().left
-                  const y = elArticle.current.getBoundingClientRect().top
-                  setPos({x, y})
+            if (elArticle.current) {
+                  let x = elArticle.current.getBoundingClientRect().left - 40
+                  if (x < 0) x = 20
+                  if (x + 350 >= window.innerWidth) x = window.innerWidth - 370
+                  let y = elArticle.current.getBoundingClientRect().top - 100
+                  if (y + 350 > window.innerHeight) y = window.innerHeight - 370
+                  if (y < 0) y = 20
+                  setPos({ x, y })
             }
       }
 
+    
+
       return (
-            <article ref={elArticle} className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'>
-                  {!isHover && <Image onMouseEnter={() => setIsHover(true)} src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
+            <article ref={elArticle}
+                  className='relative h-28 min-w-[180px] cursor-pointer ease-out md:h-36 md:min-w-[260px] '
+                  onClick={() => {
+                        setCurrentMovie(movie)
+                        setShowModal(true)
+                  }}
+                  >
+                  <Image src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
                         }`}
+                        onMouseOver={() => {
+                              debounce('mouse-enter')
+                              onSetPos()
+                        }}
                         alt="movie"
                         className="rounded-sm object-cover md:rounded"
-                        layout="fill" />}
-                  {/* {isHover && <HoverImgModal setIsHover={setIsHover} pos={pos} movie={movieDetails}/> } */}
-
+                        layout="fill" />
             </article>
       )
 }
